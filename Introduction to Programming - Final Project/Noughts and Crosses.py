a42156f70e100f3564c7c29162d6a28ff5251f98
Callum <callum.cross@hotmail.co.uk>
2024-10-23 13:55:22 +0100

    First working version, base classes and function implemented

--- Introduction to Programming - Final Project/Noughts and Crosses.py
+++ Introduction to Programming - Final Project/Noughts and Crosses.py
@@ -3,27 +3,26 @@ from string import ascii_lowercase as abc
 
 class Board:
     def __init__(self, x_size, y_size):
-        self.x_size = x_size
-        self.y_size = y_size
-        self.board_dictionary = self.generate()
+        self.x_size = x_size                                           # Sets the Y axis size of Board
+        self.y_size = y_size                                           # Sets the X axis size of Board
+        self.board_dictionary = self.generate()                        # Creates Dict for the board using the x/y size
 
-    def generate(self):                                              # Creates the Dict to store the board data
-        count = 1                                                    # used to get 1, 2, 3
-        abc_count = 0                                                # used to get a, b, c
+    def generate(self):                                                # Creates the Dict to store the board data
+        count = 1                                                      # used to get 1, 2, 3
+        abc_count = 0                                                  # used to get a, b, c
         board_dictionary = {}
         for x in range(0, self.y_size):
             for i in range(0, self.x_size):
-                board_dictionary[abc[abc_count] + str(count)] = " "  # abc[0] + 1 == a1
+                board_dictionary[abc[abc_count] + str(count)] = " "    # abc[0] + 1 == a1
                 count += 1
-            count = 1                                                # resets counter to rows so goes no higher than 3
-            abc_count += 1                                           # progresses to the next letter
+            count = 1                                                  # resets count so no higher than 3
+            abc_count += 1                                             # progresses to the next letter
         return board_dictionary
 
-    def display(self):                                              # Displays the Board GUI to the terminal
+    def display(self):                                                 # Displays the Board GUI to the terminal
         row_list = []
-        for key in self.board_dictionary.keys():                    # places the Dict values in a list
+        for key in self.board_dictionary.keys():                       # places the Dict values in a list
             row_list.append(self.board_dictionary.get(key))
-
         print(" +---+---+---+")
         print(f"C| {row_list[6]} | {row_list[7]} | {row_list[8]} |")
         print(" |---+---+---|")
@@ -34,17 +33,17 @@ class Board:
         print("   1   2   3  ")
 
 
-class Player:
-    player_count = 0
+class Player:                                                          # Keeps functions/ values for the player
+    player_count = 0                                                   # Keeps track of number of players
 
-    def __init__(self, name="VOID"):
-        self.name = name
-        Player.player_count += 1
-        self.player_number = Player.player_count
-        self.player_piece = self.get_piece()
+    def __init__(self, name="VOID"):                                   # Creates the player
+        self.name = name                                               # Gets the players name
+        Player.player_count += 1                                       # +1 for every player
+        self.player_number = Player.player_count                       # Gets player number based on order created
+        self.player_piece = self.get_piece()                           # Uses get_piece to get GUI Piece
 
     @staticmethod
-    def get_piece():
+    def get_piece():                                                   # Gets the players GUI Piece based on number
         if Player.player_count == 1:
             return "X"
         elif Player.player_count == 2:
@@ -70,33 +69,85 @@ class Game:
               "                         \n")
         input("   Press any key to start!")
 
-    def valid_placement(self, choice, piece):
+    def valid_placement(self, choice, piece):                           # Checks if piece placement is a valid location
+        # print(choice)
         for key in self.board.board_dictionary.keys():
+            # print(key)
             if choice not in self.board.board_dictionary.keys():
                 print("That is not a valid placement!\n")
-                return False
-            elif self.board.board_dictionary.get(key) != " ":
-                print("That is not a valid placement!\n")
-                return False
+                return True
+            elif self.board.board_dictionary.get(choice) != " ":
+                # print("-" + self.board.board_dictionary.get(key))
+                print("Theres already a piece there!!!\n")
+                return True
             elif choice == key:
                 self.board.board_dictionary[choice] = piece
+                #print(self.board.board_dictionary)
+                return False
+
+    @staticmethod
+    def check_victory():                                                # Check if player has met victory condition
+        victory = [["c1", "c2", "c3"], ["a1", "a2", "a3"], ["b1", "b2", "b3"],
+                   ["c1", "b1", "a1"], ["c2", "b2", "a2"], ["c3", "b3", "a3"],
+                   ["c1", "b2", "a3"], ["c3", "b2", "a1"]]
+        x_count = 0
+        o_count = 0
+        winner = ""
+        for victory_sub_list in victory:
+            for value in victory_sub_list:
+                if game.board.board_dictionary.get(value) == "X":
+                    x_count += 1
+                elif game.board.board_dictionary.get(value) == "O":
+                    o_count += 1
+            if x_count == 3:
+                winner = "X"
+                return True, winner
+            elif o_count == 3:
+                winner = "O"
+                return True, winner
+            else:
+                x_count = 0
+                o_count = 0
+        return False
+
+    def game_over(self):
+        count = 0
+        # print("Count1: " + str(count))
+        for key in game.board.board_dictionary:
+            # print("Key: " + str(key))
+            # print(game.board.board_dictionary.get(key))
+            if game.board.board_dictionary.get(key) != " ":
+                count += 1
+        if count == self.board.y_size * self.board.x_size:
+            return False
+        elif self.check_victory():
+            winner = self.check_victory()
+            if winner[1] == "X":
+                print("The Winner is Player 1!")
+            elif winner[2] == "O":
+                print("The Winner is Player 2!")
+            else:
+                print("Error! I need an Adult!")
+            return False
+        else:
+            # print("Count2: " + str(count))
             return True
 
 
 game = Game(3, 3)
 game.board.display()
-while True:
-    while True:
-        print("Player 1's Turn!")
-        game.valid_placement(game.player1.choice(), game.player1.player_piece)
-        game.board.display()
-        break
-    while True:
-        print("Player 2's Turn!")
-        game.valid_placement(game.player2.choice(), game.player2.player_piece)
-        game.board.display()
-        break
+while game.game_over():
+    print("Player 1's Turn!")
+    while game.valid_placement(game.player1.choice(), game.player1.player_piece):
 
+        game.board.display()
+    game.board.display()
+    if not game.game_over():
+        break
+    print("Player 2's Turn!")
+    while game.valid_placement(game.player2.choice(), game.player2.player_piece):
+        game.board.display()
+    game.board.display()
 
 
 
